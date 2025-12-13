48fd010d3f1b11c152446a09f318734629b804ee
Manuel Zarat <manuel.zarat@gmail.com>
2025-12-14 00:17:23 +0100

    Refactor ParserException class constructors to show caller funvtions
    Show in the error message which function has caused the crash

--- Compiler/ParserException.cs
+++ Compiler/ParserException.cs
@@ -1,49 +1,31 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
-
+using System.Runtime.CompilerServices;
 using ScriptStack.Runtime;
 
 namespace ScriptStack.Compiler
 {
-
     public class ParserException : ScriptStackException
     {
-
-        #region Private Variables
-
         private Token token;
 
-        #endregion
-
-        #region Public Methods
-
-        public ParserException()
-            : base()
-        {
-            token = null;
-        }
+        public ParserException() : base() => token = null;
 
         public ParserException(string strMessage)
-            : base("ParserException: " + strMessage)
-        {
-            token = null;
-        }
+            : base("ParserException: " + strMessage) => token = null;
 
         public ParserException(string strMessage, Exception exceptionInner)
-            : base(strMessage, exceptionInner)
-        {
-            token = null;
-        }
+            : base(strMessage, exceptionInner) => token = null;
 
-        public ParserException(string message, Token token)
-            : base(message + " Zeile " + (token.Line + 1)  + ", Zeichen "+ System.Math.Abs(token.Column - 1) + ": " + token.Text)
+        public ParserException(
+            string message,
+            Token token,
+            [CallerMemberName] string caller = "",
+            [CallerFilePath] string file = "",
+            [CallerLineNumber] int callLine = 0)
+            : base($"{message} (Caller: {caller}, {System.IO.Path.GetFileName(file)}:{callLine}) " +
+                   $"Zeile {token.Line + 1}, Zeichen {Math.Abs(token.Column - 1)}: {token.Text}")
         {
             this.token = token;
         }
-
-        #endregion
-
     }
-
 }
