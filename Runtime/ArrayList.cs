c3a6552dc700f8d4e26eacf777c96c0e1ac235f6
Manuel Zarat <manuel.zarat@gmail.com>
2025-12-27 18:27:16 +0100

    ArrayList prints arrays in brackets

--- Runtime/ArrayList.cs
+++ Runtime/ArrayList.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Globalization;
 
 namespace ScriptStack.Runtime
 {
@@ -17,7 +18,7 @@ namespace ScriptStack.Runtime
             if (objectValue.GetType() != typeof(ArrayList))
                 stringBuilder.Append("\"" + objectValue + "\"");
 
-            else 
+            else
                 stringBuilder.Append(objectValue);
 
             return;
@@ -115,22 +116,157 @@ namespace ScriptStack.Runtime
 
         public override string ToString()
         {
-            string culture = Thread.CurrentThread.CurrentCulture.NumberFormat.PercentDecimalSeparator;
-            string decimalSeparator = ",";
-            //if (culture.ToString().Trim() == ",") decimalSeparator = ";";
-            StringBuilder stringBuilder = new StringBuilder();
-            stringBuilder.Append("{ ");        
-            bool bFirst = true;
-            foreach (object objectKey in Keys)
+            // We intentionally output JSON here, because ScriptStack's "string(x)" uses Convert.ToString(x)
+            // which calls ToString(). Arrays vs. objects are distinguished by their key-shape:
+            // - purely non-negative int keys => JSON array (supports sparse arrays)
+            // - otherwise => JSON object
+            var sb = new StringBuilder();
+            var stack = new HashSet<object>(ReferenceEqualityComparer.Instance);
+            WriteJsonValue(sb, this, stack);
+            return sb.ToString();
+        }
+
+        private static void WriteJsonValue(StringBuilder sb, object value, HashSet<object> stack)
+        {
+            if (value == null || value is NullReference)
             {
-                if (!bFirst) stringBuilder.Append(decimalSeparator + " ");
-                bFirst = false;
-                OutputValue(stringBuilder, objectKey);
-                stringBuilder.Append(": ");
-                OutputValue(stringBuilder, this[objectKey]);
+                sb.Append("null");
+                return;
             }
-            stringBuilder.Append(" }");
-            return stringBuilder.ToString();
+
+            if (value is ArrayList al)
+            {
+                WriteJsonArrayList(sb, al, stack);
+                return;
+            }
+
+            switch (Type.GetTypeCode(value.GetType()))
+            {
+                case TypeCode.Boolean:
+                    sb.Append(((bool)value) ? "true" : "false");
+                    return;
+                case TypeCode.String:
+                    WriteJsonString(sb, (string)value);
+                    return;
+                case TypeCode.Char:
+                    WriteJsonString(sb, value.ToString());
+                    return;
+                case TypeCode.Int16:
+                case TypeCode.Int32:
+                case TypeCode.Int64:
+                case TypeCode.UInt16:
+                case TypeCode.UInt32:
+                case TypeCode.UInt64:
+                case TypeCode.Single:
+                case TypeCode.Double:
+                case TypeCode.Decimal:
+                case TypeCode.SByte:
+                case TypeCode.Byte:
+                    // JSON numbers must be culture-invariant.
+                    sb.Append(Convert.ToString(value, CultureInfo.InvariantCulture));
+                    return;
+                case TypeCode.DateTime:
+                    // No dedicated JSON date type: emit as ISO-8601 string.
+                    WriteJsonString(sb, ((DateTime)value).ToString("O", CultureInfo.InvariantCulture));
+                    return;
+                default:
+                    // Fallback: emit as string.
+                    WriteJsonString(sb, value.ToString());
+                    return;
+            }
+        }
+
+        private static void WriteJsonArrayList(StringBuilder sb, ArrayList al, HashSet<object> stack)
+        {
+            if (!stack.Add(al))
+            {
+                // Cyclic structure: represent as null to avoid infinite recursion.
+                sb.Append("null");
+                return;
+            }
+
+            // Detect "array" shape: all keys are non-negative int.
+            bool allIntKeys = true;
+            int maxIndex = -1;
+            foreach (var k in al.Keys)
+            {
+                if (k is int i && i >= 0)
+                {
+                    if (i > maxIndex) maxIndex = i;
+                    continue;
+                }
+
+                allIntKeys = false;
+                break;
+            }
+
+            if (allIntKeys)
+            {
+                sb.Append('[');
+                for (int i = 0; i <= maxIndex; i++)
+                {
+                    if (i > 0) sb.Append(',');
+                    if (al.TryGetValue(i, out var v))
+                        WriteJsonValue(sb, v, stack);
+                    else
+                        sb.Append("null");
+                }
+                sb.Append(']');
+            }
+            else
+            {
+                sb.Append('{');
+                bool first = true;
+                foreach (var kv in al)
+                {
+                    if (!first) sb.Append(',');
+                    first = false;
+                    WriteJsonString(sb, kv.Key?.ToString() ?? "null");
+                    sb.Append(':');
+                    WriteJsonValue(sb, kv.Value, stack);
+                }
+                sb.Append('}');
+            }
+
+            stack.Remove(al);
+        }
+
+        private static void WriteJsonString(StringBuilder sb, string s)
+        {
+            sb.Append('"');
+            if (s != null)
+            {
+                foreach (char c in s)
+                {
+                    switch (c)
+                    {
+                        case '"': sb.Append("\\\""); break;
+                        case '\\': sb.Append("\\\\"); break;
+                        case '\b': sb.Append("\\b"); break;
+                        case '\f': sb.Append("\\f"); break;
+                        case '\n': sb.Append("\\n"); break;
+                        case '\r': sb.Append("\\r"); break;
+                        case '\t': sb.Append("\\t"); break;
+                        default:
+                            if (c < 0x20)
+                                sb.Append("\\u" + ((int)c).ToString("x4"));
+                            else
+                                sb.Append(c);
+                            break;
+                    }
+                }
+            }
+            sb.Append('"');
+        }
+
+        /// <summary>
+        /// Reference equality comparer for cycle detection.
+        /// </summary>
+        private sealed class ReferenceEqualityComparer : IEqualityComparer<object>
+        {
+            public static readonly ReferenceEqualityComparer Instance = new ReferenceEqualityComparer();
+            public new bool Equals(object x, object y) => ReferenceEquals(x, y);
+            public int GetHashCode(object obj) => System.Runtime.CompilerServices.RuntimeHelpers.GetHashCode(obj);
         }
 
         #endregion
